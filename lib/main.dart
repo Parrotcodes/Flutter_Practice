77ac9020586a75c3fef9b8b2ee946f9d2bfbf832
rakesh <atrimyself@gmail.com>
2025-04-26 15:16:39 +0530

    First flutter project done --> I Am Rich (App)

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "I Am Rich",
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ),
+          backgroundColor: Colors.blue[500],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image(
+                // image: NetworkImage("https://uploads.ibomma.studio/480956163.jpg"),
+                image: AssetImage("assets/Images/bgdiamond.gif"),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "IAMRich-Brand",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text("copyright-2025"),
+          ],
+        ),
+      ),
+    ),
+  );
+}
