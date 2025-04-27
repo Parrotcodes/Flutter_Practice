767a35bdbf36f8ceb19fc397c06d5e1f2b72f801
rakesh <atrimyself@gmail.com>
2025-04-27 13:40:45 +0530

    updated main with stless widget Home Class

--- lib/main.dart
+++ lib/main.dart
@@ -4,38 +4,46 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blue[100],
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              "I Am Rich",
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-            ),
-          ),
-          backgroundColor: Colors.blue[500],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image(
-                // image: NetworkImage("https://uploads.ibomma.studio/480956163.jpg"),
-                image: AssetImage("assets/Images/bgdiamond.gif"),
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              "IAMRich-Brand",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text("copyright-2025"),
-          ],
-        ),
-      ),
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "I Am Rich",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.blue[500],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image(
+              // image: NetworkImage("https://uploads.ibomma.studio/480956163.jpg"),
+              image: AssetImage("assets/Images/bgdiamond.gif"),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "IAMRich-Brand",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text("copyright-2025"),
+        ],
+      ),
+    );
+  }
 }
